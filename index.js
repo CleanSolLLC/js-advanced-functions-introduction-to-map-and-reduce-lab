c89388ed37fe13467639b2296ee3d0b7f6b4c834
mamark90 <mamark90@gmail.com>
2021-08-12 18:41:48 -0400

    commit tree af79ec17b5d8b715ab1ca54e7ddbb679298ff5a9

--- index.js
+++ index.js
@@ -1,1 +1,79 @@
-// Your code here
+function mapToNegativize(sourceArray) {
+ let newArray = [];	
+ for (const num of sourceArray) {
+ 	newArray.push(num * -1);
+ }
+ return newArray;	
+}
+
+function mapToNoChange(sourceArray) {
+  let newArray = [];	
+  for (let i = 0; i < sourceArray.length; i++) {
+	newArray.push(sourceArray[i]);
+  }  
+  return newArray;
+}
+
+
+function mapToDouble(sourceArray) {
+ let newArray = [];	
+ for (const num of sourceArray) {
+ 	newArray.push(num * 2);
+ }
+ return newArray;	
+}
+
+
+
+function mapToSquare(sourceArray) {
+ let newArray = [];	
+ for (const num of sourceArray) {
+ 	newArray.push(num ** 2);
+ }
+ return newArray;	
+}
+
+
+
+function reduceToTotal(sourceArray, startingPoint) {
+	let total = startingPoint === undefined ? 0 : startingPoint;
+	for (const num of sourceArray) {
+		total += num;
+	}
+	return total;
+}
+
+
+const falsyValues = [false, null, undefined, 0, -0, NaN, ""]
+
+
+function reduceToAllTrue(sourceArray) {
+	let array1 = []
+	//let array2 = sourceArray
+	for (const value of sourceArray) {
+		debugger;
+	  if (falsyValues.includes(value)) {
+	    array1.push(value)
+	  }
+	}
+	  if (array1.length > 0) {
+	  	return false;
+	  }else 
+	  return true;
+}
+
+
+function reduceToAnyTrue(sourceArray) {
+	let array1 = [];
+	for (const value of sourceArray) {
+	  if (!falsyValues.includes(value)) {
+	    array1.push(value)
+	  }
+
+	}
+	  if (array1.length > 0) {
+	  	return true;
+	  }else 
+	  return false;
+}
+
